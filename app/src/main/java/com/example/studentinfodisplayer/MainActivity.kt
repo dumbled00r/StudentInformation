28867e5ed0907555302e33cce1d71accd527d1b7
duyanh3101 <kazuu.promotion@gmail.com>
2023-10-31 01:25:45 +0700

    Finishing code

--- app/src/main/java/com/example/studentinfodisplayer/MainActivity.kt
+++ app/src/main/java/com/example/studentinfodisplayer/MainActivity.kt
@@ -11,18 +11,18 @@ class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Binding
+        // create binding for easier access
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        // Create a list that holds information of a student
         val lstStudent = mutableListOf<StudentModel>()
         // Generate Student's data
         for (i in 1..1000){
             val studentInfo = StudentModel("Student $i", i)
             lstStudent.add(studentInfo)
         }
-
+        // Create recycler view and use the adapter to make it view the list of student
         val recyclerView = binding.recyclerView
         recyclerView.adapter = ItemAdapter(this, lstStudent)
         recyclerView.setHasFixedSize(true)
--- app/src/main/java/com/example/studentinfodisplayer/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/studentinfodisplayer/adapter/ItemAdapter.kt
@@ -14,12 +14,14 @@ class ItemAdapter(
     private val context:Context,
     private val dataset : List<StudentModel>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
         class ItemViewHolder(private val view: View): ViewHolder(view){
+            // Find the text view
             val studentNameTextView : TextView = view.findViewById(R.id.studentName)
             val studentIdTextView : TextView = view.findViewById(R.id.studentId)
 
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // get adapter layout for the list item
         val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
 
         return ItemViewHolder(adapterLayout)
@@ -29,6 +31,9 @@ class ItemAdapter(
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
+        /* Here we set the student's name and id to each corresponding data line
+         * I also formatted a type of user id that is input into the student id text view
+         */
         holder.studentNameTextView.text = item.studentName
         holder.studentIdTextView.text = context.resources.getString(R.string.student_id_123, "HUST" + item.studentId)
     }
